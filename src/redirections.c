c544a970f38329d14be4df918c20f2789f99b0b8
BlasDeLezoEsGod <arnaucebrianortega@gmail.com>
2025-03-24 19:22:22 +0100

    beta

--- src/redirections.c
+++ src/redirections.c
@@ -52,9 +52,11 @@ int redir_first_child(t_redirection *redir, t_shell *shell)
 	{
 		if (tmp->type == REDIR_IN && set_input_redir(tmp, tmp->fd_in, 0))
 			return (1);
-		if ((tmp->type == REDIR_OUT || tmp->type == APPEND) 
-				&& set_output_redir(tmp, tmp->fd_out, 0))
-			return (1);
+		if ((tmp->type == REDIR_OUT || tmp->type == APPEND))
+		{
+			if (set_output_redir(tmp, tmp->fd_out, 0))
+				return (1);
+		}
 		else
 		{
 			if (set_output_redir(tmp, shell->pipes[1], 0))
@@ -68,21 +70,26 @@ int redir_first_child(t_redirection *redir, t_shell *shell)
 int	redir_last_child(t_redirection *redir, t_shell *shell)
 {
 	t_redirection	*tmp;
+	t_redirection	*last_out;
 
+	last_out = NULL;
 	tmp = redir;
 	while (tmp)
 	{
 		tmp->flag_in = 0;
 		if (tmp->type == REDIR_IN && set_input_redir(tmp, tmp->fd_in, 1))
 			return (1);
-		else if (tmp->type != REDIR_IN && tmp->flag_in == 0 
-				&& set_input_redir(tmp, shell->pipes[2 * shell->child - 2], 1))
-			return (1);
-		if ((tmp->type == REDIR_OUT || tmp->type == APPEND) 
-				&& set_output_redir(tmp, tmp->fd_out, 0))
-			return (1);
+		else if (tmp->type != REDIR_IN && tmp->flag_in == 0)
+		{	
+			if (set_input_redir(tmp, shell->pipes[2 * shell->child - 2], 1))
+				return (1);
+		}
+		if (tmp->type == REDIR_OUT || tmp->type == APPEND)
+			last_out = tmp;
 		tmp = tmp->next;
 	}
+	if (last_out && set_output_redir(last_out, last_out->fd_out, 0))
+		return (1);
 	return (0);	
 }
 
